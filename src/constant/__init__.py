b272a0af4e6731c862ada02a786e26f198181700
abhishek97amathe <avinashamathe@gmail.com>
2025-01-26 16:07:30 +0530

    New files

---
+++ src/constant/__init__.py
@@ -0,0 +1,17 @@
+import os
+
+
+AWS_S3_BUCKET_NAME = "wafer-fault"
+MONGO_DATABASE_NAME = "My_db"
+MONGO_COLLECTION_NAME = "wafer"
+
+
+TARGET_COLUMN = "quality"
+MONGO_DB_URL="mongodb+srv://abhishekamathe:archer97@cluster0.klw8d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+
+
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXTENSION = ".pkl"
+
+artifact_folder =  "artifacts"
